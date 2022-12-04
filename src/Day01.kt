b7ab4e43c3bc8a1f00a149e766e785ccb3fcfc40
ya-nesh <16968166+ya-nesh@users.noreply.github.com>
2022-12-03 20:05:46 -0500

    Finish Day01 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val map = mutableMapOf<Int, Int>()
+        val elfSequence = generateSequence(1) { it + 1 }.iterator()
+
+        var currentElf: Int = elfSequence.next()
+
+        for (line in input) {
+            if (line.isEmpty()) {
+                currentElf = elfSequence.next()
+            } else {
+                map[currentElf] = map.getOrDefault(currentElf, 0) + line.toInt()
+            }
+        }
+
+        return map.values.max()
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val testInput = readInput("Day01")
+    println(part1(testInput))
 }
