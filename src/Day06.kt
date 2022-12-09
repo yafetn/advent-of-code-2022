11ef9a074a6a00060c7c38ca09ca0bdd9e65d586
ya-nesh <16968166+ya-nesh@users.noreply.github.com>
2022-12-08 20:19:16 -0500

    Finish Day06 part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -21,11 +21,28 @@ fun main() {
     }
 
 
-    fun part2(input: List<String>): String {
-        TODO()
+    fun part2(input: List<String>): Int {
+        var result = -1
+
+        for (line in input) {
+            var right = 14 - 1
+            var left = 0
+
+            for(i in right until line.length) {
+                val window = line.slice(left..right).toSet()
+                if (window.size == 14) {
+                    result = i + 1
+                    break
+                }
+                right++
+                left++
+            }
+        }
+
+        return result
     }
 
     val testInput = readInput("Day06")
     println(part1(testInput))
-    // println(part2(testInput))
+    println(part2(testInput))
 }
